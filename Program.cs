4c436f33ed6b7e6de5529b91901cb3d85e23ac93
hoangh <hoangh@amazon.com>
2021-10-14 13:18:04 +0700

    fix program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,22 @@
-using System;
-using System.Diagnostics;
-using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace highmem_test;
 
 class Program
 {
-    private static readonly int ProcId = Process.GetCurrentProcess().Id;
-
     public static async Task<int> Main(string[] args)
     {
-        if (args.Length != 2)
-        {
-            Console.Error.WriteLine("Usage: ./highmem_test <file> <size>");
-            return 1;
-        }
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .UseSystemd()
+            .Build();
 
-        using var fileHandle = File.OpenHandle(args[0], FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-        var nread = 0;
-        var total = 0;
-        var toRead = long.Parse(args[1]);
-
-        var buf = new byte[1024];
-        while (total < toRead && (nread = await RandomAccess.ReadAsync(fileHandle, buf, 0)) > 0)
-        {
-            total += nread;
-        }
-
-        using var p = Process.Start("/bin/sh", $"cat /proc/ProcId/status | grep RssAnon");
-        p.Start();
-        string stdoutContent = p.StandardOutput.ReadToEnd();
-        Console.WriteLine(stdoutContent);
-        Console.ReadLine();
+        await host.RunAsync();
         return 0;
     }
 }
