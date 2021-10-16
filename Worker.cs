10e2e9fc3760c8d2a7e755df1ab37ea48f6bad1e
hoangh <hoangh@amazon.com>
2021-10-16 17:08:43 +0700

    add tasks

--- Worker.cs
+++ Worker.cs
@@ -1,8 +1,9 @@
-namespace highmem_test;
+﻿namespace highmem_test;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly HttpClient _httpClient = new HttpClient();
 
     public Worker(ILogger<Worker> logger)
     {
@@ -11,10 +12,35 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var tasks = new Task[5];
+        for (var i = 0; i < tasks.Length; i++)
+        {
+            tasks[i] = MakeHttpRequest(stoppingToken);
+        }
+
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(1000, stoppingToken);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task MakeHttpRequest(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            using var response = await _httpClient.GetAsync("https://api.github.com/users/dhhoang");
+            //_logger.LogInformation("Response code {ResponseCode}", response.StatusCode);
+            await Task.Delay(1000, cancellationToken);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
         }
     }
 }
